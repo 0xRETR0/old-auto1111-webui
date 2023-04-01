12b92aea659f10799c712abc69055201e6f399ef
0xRETR0 <w.pasieczynsk08@gmail.com>
2023-04-01 17:40:55 +0200

    remove some stuff
    i really dont know

--- extensions/openOutpaint-webUI-extension/preload.py
+++
@@ -1,6 +0,0 @@
-import argparse
-
-def preload(parser: argparse.ArgumentParser):
-        parser.add_argument("--lock-oo-submodule", action='store_true',
-                        help="(openOutpaint-webUI-extension) Prevent checking for main openOutpaint submodule updates.")
-        
